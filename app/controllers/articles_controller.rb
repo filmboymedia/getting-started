e908b3600b17da772cb1eb4e9f4c84ab40ac06f5
Edgar Alcantara <edgaralcantara@mac-wifi.local>
2015-04-10 15:15:08 -0500

    just started repo for this

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,45 @@
+class ArticlesController < ApplicationController
+	def index
+    @articles = Article.all
+  end
+
+	#This will generate each post "[:id]" will be replaced with post ID number
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+	  @article = Article.new
+	end
+
+	def edit
+  	@article = Article.find(params[:id])
+	end
+
+	def create
+	  @article = Article.new(article_params)
+	 
+	  if @article.save
+    	redirect_to @article
+	  else
+	    render 'new'
+	  end
+	end
+
+	def update
+	  @article = Article.find(params[:id])
+	 
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
+	end
+
+	private
+	  def article_params
+	  	# ".permit()" is used for security...you need to explicitly tell tails what fields can be accepted. 
+			# This helps keep hackers from creating their own.
+	    params.require(:article).permit(:title, :text, :author, :date)
+	  end
+end
